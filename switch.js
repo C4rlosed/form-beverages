81872d19b2867da5a091df083e2ee15f0878b90f
Carlos Eduardo Pereira Bento <107080282+C4rlosed@users.noreply.github.com>
2023-04-11 16:08:11 -0300

    Delete switch.js

--- switch.js
+++
@@ -1,8 +0,0 @@
-function toggleColors() {
-    var body = document.getElementsByTagName("body")[0];
-    var container = document.getElementsByClassName("container")[0];
-    body.classList.toggle("cor1");
-    container.classList.toggle("cor1");
-    body.classList.toggle("cor2");
-    container.classList.toggle("cor2");
-}
